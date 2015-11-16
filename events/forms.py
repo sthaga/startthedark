484d0a20a9db0dacf0c443abab20cf09354ddf2d
sthaga <otonake@gmail.com>
2015-11-17 01:25:04 +0900

    6th

--- events/forms.py
+++ events/forms.py
@@ -4,6 +4,6 @@ from events.models import Event
 class EventForm(forms.ModelForm): 
 	description = forms.CharField(max_length=340, widget=forms.Textarea)
 
- 	class Meta: 
+	class Meta: 
 		model = Event 
 		fields = ('description',) 
--- events/views.py
+++ events/views.py
@@ -1,14 +1,22 @@
-from events.models import Event
-from django.shortcuts import render_to_response
+from events.models import Event, Attendance
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from events.forms import EventForm
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
+from django.contrib.auth.decorators import login_required
 
 def tonight(request):
 	events = Event.objects.today().filter(latest=True)
+	attending = []
+	for event in events:
+		try:
+			Attendance.objects.get(event=event, user=request.user)
+			attending.append(True)
+		except Attendance.DoesNotExist:
+			attending.append(False)
 	context = {
-		'events': events,
+		'events': zip(events, attending),
 	}
 	return render_to_response(
 		'events/tonight.html',
@@ -41,3 +49,25 @@ def create(request):
 		{'form': form},
 		context_instance = RequestContext(request)
 	)
+
+create = login_required(create)
+
+def toggle_attendance(request):
+	try:
+		event_id = int(request.POST['event_id'])
+	except (KeyError, ValueError):
+		raise Http404
+
+	event = get_object_or_404(Event, id=event_id)
+	attendance, created = Attendance.objects.get_or_create(user=request.user, event=event)
+	if created:
+		request.user.message_set.create(message='You are now attending "%s"' %event)
+	else:
+		attendance.delete()
+		request.user.message_set.create(message='You are no longer attending "%s"' %event)
+	next = request.POST.get('next', '')
+	if not next:
+		next = reverse('ev_tonight')
+	return HttpResponseRedirect(next)
+
+toggle_attendance = login_required(toggle_attendance)
