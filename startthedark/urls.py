e2054a2d0d48c4af0d04b1f5631b47141b1351ba
sthaga <otonake@gmail.com>
2015-11-17 22:05:13 +0900

    9th

--- startthedark/urls.py
+++ startthedark/urls.py
@@ -4,5 +4,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'^friends/', include('socialgraph.urls')),
 	url(r'^events/', include('events.urls')),
 )
