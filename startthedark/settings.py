7b7507b45f5c93e8722e47582b895ad614036d8d
sthaga <otonake@gmail.com>
2015-11-17 21:30:34 +0900

    8th

--- startthedark/settings.py
+++ startthedark/settings.py
@@ -32,41 +32,41 @@ ALLOWED_HOSTS = []
 TEMPLATES = [ 
 { 
 	'BACKEND': 'django.template.backends.django.DjangoTemplates',
-	'DIRS': [os.path.join(BASE_DIR, 'templates'),
-		os.path.join(BASE_DIR, 'templates/events')], 
+	'DIRS': [os.path.join(BASE_DIR, 'templates')], 
 	'APP_DIRS': True,
 	'OPTIONS': { 
 		'context_processors': [
-		'django.template.context_processors.debug', 
-		'django.template.context_processors.request', 
-		'django.contrib.auth.context_processors.auth',
-		'django.contrib.messages.context_processors.messages', 
-             ], 
-         }, 
-     }, 
- ] 
+			'django.template.context_processors.debug', 
+			'django.template.context_processors.request', 
+			'django.contrib.auth.context_processors.auth',
+			'django.contrib.messages.context_processors.messages', 
+			],
+		},
+	},
+]
 
 
 
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'events',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'events',
+	'socialgraph',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'startthedark.urls'
